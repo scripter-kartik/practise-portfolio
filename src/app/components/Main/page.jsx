befd8fb154f8f169fb43f0e4a32f14978b0bece8
Kartik <agarwalkartik704@gmail.com>
2026-03-15 22:35:14 +0530

    Update page.jsx

--- src/app/components/Main/page.jsx
+++ src/app/components/Main/page.jsx
@@ -1,7 +1,8 @@
-// main jsx
-"use client";
+
+      "use client";
+
 import Link from "next/link";
-//importing motion 
+import Image from "next/image";
 import { motion } from "framer-motion";
 
 export default function Main() {
@@ -11,30 +12,34 @@ export default function Main() {
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: -300, opacity: 0 }}
       transition={{ duration: 1 }}
-      className="flex justify-center flex-col"
+      className="flex flex-col justify-center px-6 md:px-16"
     >
       <div>
-        <p className="font-mono text-sm md:text-2xl text-gray-500 mt-20 mb-5 md:mt-30 md:mb-5">
+        <p className="font-mono text-sm md:text-2xl text-gray-500 mt-20 mb-5">
           Hello! I'm Kartik
         </p>
-        <div className="text-sm md:text-[97.5px] font-extrabold flex flex-col justify-center gap-0 leading-none">
-          <h1 className="m-0">I HELP BUSINESSES BUILD FAST,</h1>
-          <h1 className="m-0">SCALABLE AND STUNNING</h1>
-          <h1 className="text-gray-400 m-0 mb-10">WEB PRODUCTS.</h1>
+
+        <div className="text-2xl md:text-[80px] font-extrabold flex flex-col leading-none">
+          <h1>I HELP BUSINESSES BUILD FAST,</h1>
+          <h1>SCALABLE AND STUNNING</h1>
+          <h1 className="text-gray-400 mb-10">WEB PRODUCTS.</h1>
         </div>
       </div>
-      <div className="flex justify-between items-center mt-30 w-[calc(100vw - 100px)]">
-        <Link href="/Contact">
-          <button className="bg-black w-50 h-15 border-2 border-black text-white text-2xl flex justify-center items-center gap-2 rounded-4xl hover:cursor-pointer">
-            let's talk{" "}
-            <img
-              className="w-10 h-10 text-white"
+
+      <div className="flex flex-col md:flex-row justify-between items-center gap-10 mt-20 w-full">
+        <Link href="/contact">
+          <button className="bg-black px-8 py-4 border-2 border-black text-white text-lg md:text-2xl flex items-center gap-3 rounded-full hover:opacity-80 transition">
+            let's talk
+            <Image
               src="/white-arrow.png"
-              alt=""
+              alt="arrow"
+              width={40}
+              height={40}
             />
           </button>
         </Link>
-        <div className="flex flex-col font-mono text-2xl">
+
+        <div className="flex flex-col font-mono text-lg md:text-2xl text-center md:text-left">
           <p>Performance-driven full-stack development for</p>
           <p>modern digital products</p>
         </div>
